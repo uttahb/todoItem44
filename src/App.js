3d03600ad14d3c09ac6e53e1740906cecdba6fdb
Jishnu <jishnunbhattathiri@gmail.com>
2022-07-22 15:32:13 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import TodoItem from './todoItem44'
+
+export default function App(props) {
+  return (
+    <div>
+      <TodoItem item="sdfsdf" id="sdfsdf" />
+    </div>
+  )
+}
